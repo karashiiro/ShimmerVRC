57177fd835bb49a8e9384b173cc430698a41ff4b
karashiiro <49822414+karashiiro@users.noreply.github.com>
2025-05-11 12:05:40 -0700

    Initial Commit

---
+++ ShimmerVRC/ShimmerVRCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerVRCApp.swift
+//  ShimmerVRC
+//
+//  Created by karashiiro on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerVRCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShimmerVRCTests/ShimmerVRCTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerVRCTests.swift
+//  ShimmerVRCTests
+//
+//  Created by karashiiro on 5/11/25.
+//
+
+import Testing
+@testable import ShimmerVRC
+
+struct ShimmerVRCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
