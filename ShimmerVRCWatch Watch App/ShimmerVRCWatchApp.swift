04cd5bdbaa3bf6480654b1345c387353d23b742b
karashiiro <49822414+karashiiro@users.noreply.github.com>
2025-05-11 21:43:25 -0700

    Create watchOS project

---
+++ ShimmerVRCWatch Watch App/ShimmerVRCWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerVRCWatchApp.swift
+//  ShimmerVRCWatch Watch App
+//
+//  Created by karashiiro on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerVRCWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShimmerVRCWatch Watch AppTests/ShimmerVRCWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerVRCWatch_Watch_AppTests.swift
+//  ShimmerVRCWatch Watch AppTests
+//
+//  Created by karashiiro on 5/11/25.
+//
+
+import Testing
+@testable import ShimmerVRCWatch_Watch_App
+
+struct ShimmerVRCWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
